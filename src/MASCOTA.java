1d3d2df364d9eb0b44a0254d7c846051c904370a
fanny gutierrez <omoyano@ucundinamarca.edu.co>
2026-02-23 21:44:59 -0500

    proyectoclase

---
+++ src/MASCOTA.java
@@ -0,0 +1,83 @@
+public class MASCOTA {
+
+    private String nombre;
+    private String especie;
+    private int edad;
+    private double peso;
+    private boolean vacunado;
+
+    //CONSTRUCTOR VACIO
+
+    public MASCOTA() {
+    }
+
+    // CONSTRUCTOR CON PARAMETROS
+
+    public MASCOTA(String nombre, String especie, int edad, double peso) {
+        this.nombre = nombre;
+        this.especie = especie;
+        this.edad = edad;
+        this.peso = peso;
+        this.vacunado = false;
+
+    }
+
+    // GETTER-SETTER
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public boolean isVacunado() {
+        return vacunado;
+    }
+
+    public void setVacunado(boolean vacunado) {
+        this.vacunado = vacunado;
+    }
+
+    //TOSTRING
+
+
+    @Override
+    public String toString() {
+        return "MASCOTA{" +
+                "nombre='" + nombre + '\'' +
+                ", especie='" + especie + '\'' +
+                ", edad=" + edad +
+                ", peso=" + peso +
+                ", vacunado=" + vacunado +
+                '}';
+    }
+
+
+}
